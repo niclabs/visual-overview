1e76361a13bf09f2e45be5255ccef95644a42542
David Muñoz <dmunoz@pudge.niclabs.cl>
2014-08-22 11:13:33 -0400

    Changed CSV to DSV
     - Now the csv archives can have different delimiters.
     - Improved test2.html, now it can receive an URL with a dataset. Results are still shown on console.
     - Upgraded d3.js to latest version (old one didn't supported dsv).

--- js/csv_utils.js
+++ js/csv_utils.js
@@ -1,25 +1,65 @@
-function detectHeader(url){
-	var headers = [];
-	var annotations = [];
+function determineSeparator(url){
+	var separatorList = [",",";","|","	"];
+	var separator = "";
+	var sPosition = 0;
+	var maxOccurrence = 0;
 	d3.text(url, function(data){
-		var rows = d3.csv.parseRows(data);
-		var length = rows.length;
-		console.log(length);
-		for(var i=0; i<length; i++){
-			if(isHeader(rows[i])){
-				annotations = rows.splice(0,i);
-				headers = rows.shift();
-				break;
+		if(data == null){
+			console.log("Couldn't parse data");
+		}
+		var sample = data.substring(0,500);
+		for(var i=0; i < separatorList.length; i++){
+			separator = separatorList[i];
+			var occurrence = occurrences(sample, separator);
+			if(occurrence > maxOccurrence){
+				maxOcurrence = occurrence;
+				sPosition = i;
 			}
 		}
+		separator = separatorList[sPosition];
+		detectHeader(data, separator);
+	})
+
+}
+
+/* Sacado de http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string */
+
+function occurrences(string, subString){
+
+    string+=""; subString+="";
+    if(subString.length<=0) return string.length+1;
+
+    var n=0, pos=0;
+    var step= 1;
+
+    while(true){
+        pos=string.indexOf(subString,pos);
+        if(pos>=0){ n++; pos+=step; } else break;
+    }
+    return(n);
+}
+
+function detectHeader(data, separator){
+	var headers = [];
+	var annotations = [];
+	var dsv = d3.dsv(separator, "text/plain");
+	var rows = dsv.parseRows(data);
+	var length = rows.length;
+	for(var i=0; i<length; i++){
+		if(isHeader(rows[i])){
+			annotations = rows.splice(0,i);
+			headers = rows.shift();
+			break;
+		}
+	}
 		console.log(annotations);
 		console.log(headers);
 		console.log(rows);
-	});
+
 }
 
 function isHeader(row){
-	if(containsEmpty(row) || containsNumbers(row))
+	if(containsEmpty(row))
 		return false;
 	return true;
 }
@@ -33,16 +73,4 @@ function containsEmpty(row){
 		}
 	}
 	return isEmpty;
-}
-
-function containsNumbers(row){
-	var hasNumber = false;
-	var regex = /\d/;
-	for(var i=0; i<row.length; i++){
-		if(regex.test(row[i])){
-			hasNumber = true;
-			break;
-		}
-	}
-	return hasNumber;
 }
