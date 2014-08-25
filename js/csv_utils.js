54852a49b24e39940bd97b065c20a4c1a7e7e033
David Muñoz <dmunoz@pudge.niclabs.cl>
2014-08-25 11:13:57 -0400

    Footer detection added
    Now the footer (if any) can be detected. This is accomplished using the same heuristics of header.
    
    Also, there is a minor fix on determineSeparator: The function occurrences has been deleted, and the separator counting is done via javascript's string.split()

--- js/csv_utils.js
+++ js/csv_utils.js
@@ -5,23 +5,15 @@ function determineSeparator(url){
 	var maxOccurrence = 0;
 	d3.text(url, function(data){
 		if(data == null){
-			console.log("Couldn't parse data");
+			alert("Couldn't parse dataset");
+			return;
 		}
 		var sample = data.substring(0,50000);
-		/*for(var i=0; i < separatorList.length; i++){
-			separator = separatorList[i];
-			var occurrence = sample.split(separator).length;
-			console.log(occurrence);
-			if(occurrence > maxOccurrence){
-				maxOcurrence = occurrence;
-				sPosition = i;
-			}
-		}*/
 		for(var i=0; i < separatorList.length; i++){
-			separator = separatorList[i];
-			var occurrence = occurrences(sample, separator);
+			separator = separatorList[i];			
+			var occurrence = sample.split(separator).length;
 			if(occurrence > maxOccurrence){
-				maxOcurrence = occurrence;
+				maxOccurrence = occurrence;
 				sPosition = i;
 			}
 		}
@@ -31,27 +23,11 @@ function determineSeparator(url){
 
 }
 
-/* Sacado de http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string */
-
-function occurrences(string, subString){
-
-    string+=""; subString+="";
-    if(subString.length<=0) 
-	return string.length+1;
-
-    var n=0, pos=0;
-
-    while(true){
-        pos=string.indexOf(subString,pos);
-        if(pos>=0){ n++; pos+=1; } else break;
-    }
-    return(n);
-}
-
 function detectHeader(data, separator){
 	var annotations = [];
 	var dsv = d3.dsv(separator, "text/plain");
 	var rows = dsv.parseRows(data);
+	var footer = [];
 	var length = rows.length;
 	for(var i=0; i<length; i++){
 		if(isHeader(rows[i])){
@@ -59,8 +35,10 @@ function detectHeader(data, separator){
 			break;
 		}
 	}
+	footer = detectFooter(rows);
 	console.log(annotations);
 	console.log(rows);
+	console.log(footer);
 
 }
 
@@ -84,4 +62,19 @@ function containsEmpty(row, maxEmpty){
 		isEmpty = true;
 	}
 	return isEmpty;
+}
+
+function detectFooter(rows){
+	var footer = [];
+	rows.reverse();
+	var length = rows.length;
+	for(var i=0; i<length; i++){
+		if(isHeader(rows[i])){
+			footer = rows.splice(0,i);
+			break;
+		}
+	}
+	rows.reverse();
+	footer.reverse();
+	return footer;
 }
